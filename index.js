369bcb1e67e9293ec24dcd0dd2ef86d8e3c3f71c
klwidmer <30774130+klwidmer@users.noreply.github.com>
2018-02-27 22:23:21 -0600

    practice1

---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+// const sentence = 'this is a string';
+
+// let happy = true;
+
+// console.log(typeof sentence, sentence);
+// console.log(typeof happy, happy);
+// console.log(typeof age, age);
+// console.log(typeof hairColor, hairColor);
+
+// const name = 'josh';
+// let happy = true;
+// let age = 33;
+// const hairColor = 'red';
+
+// const josh = [name, happy, age, hairColor, ['kids', 'dogs', 4]];
+
+// for (let index = 0; index < josh.length; index++){
+// console.log(josh[index]);
+// }
+
+// const Josh ={
+//     name: 'Josh',
+//     age: 33,
+//     happy: false,
+//     hairColor: 'red'
+// };
+// const Alex ={
+//     name: 'Alex',
+//     age: 25,
+//     happy: true,
+//     hairColor: 'black'
+// };
+// const Kevin ={
+//     name: 'Kevin',
+//     age: 31,
+//     happy: true,
+//     hairColor: 'brown',
+// };
+
+// const studentArray =[Josh, Alex, Kevin];
+// console.log(studentArray);
+
+// for (let i = 0; i < studentArray.length; i++){
+//     console.log(`Hello, ${studentArray[i].name}. I heard you are ${studentArray[i].age}. That sure is old! Why are you so ${studentArray[i].happy ? 'happy' :'unhappy'}?`);
+// }
+
+// if(Josh.happy) {
+//     return 'happy';
+// } else {
+//     return 'unhappy';
+// }
+
